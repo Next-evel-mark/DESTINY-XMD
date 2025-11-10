515c546f018332903bdac1d1d0d1675db4c5341b
Tech_Mark <marksimiyu002@gmail.com>
2025-11-10 15:42:11 +0300

    Create session.js

---
+++ variable/session.js
@@ -0,0 +1,7 @@
+module.exports = {
+  SESSION_ID: "PUT_YOUR_SESSION_ID_HERE", // ← Users paste their session ID here
+  BOT_NAME: "Mark-MD",
+  PREFIX: "!",
+  OWNER_NUMBER: "2547XXXXXXXX",
+  SUDO_NUMBERS: "2547XXXXXXXX,2541XXXXXXXX"
+};
