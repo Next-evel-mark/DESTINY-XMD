ae79e8bc501814098b12f64a6707baec55289867
Tech_Mark <marksimiyu002@gmail.com>
2025-11-10 19:44:06 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,18 @@
+{
+  "name": "mark-md",
+  "version": "1.0.0",
+  "description": "DESTINY-XMD WhatsApp bot based on baileys contribution",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "@whiskeysockets/baileys": "latest",
+    "express": "^4.18.2",
+    "pino": "^8.14.0"
+  },
+  "engines": {
+    "node": ">=18.x",
+    "npm": ">=9.x"
+  }
+}
