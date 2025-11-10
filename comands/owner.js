9d7810da1ba679433acfab32743ab15f16832b57
Tech_Mark <marksimiyu002@gmail.com>
2025-11-10 15:51:01 +0300

    Delete comands directory

--- comands/owner.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: "owner",
-  async execute(sock, msg, args, from) {
-    await sock.sendMessage(from, {
-      text: `👑 *Owner:* MARK TECH\n📞 *Contact:* wa.me/254110550356\n💬 *Bot:* DESTINY-XMD`,
-    });
-  },
-};
