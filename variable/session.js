5cd2699f410bb9a2ef5c8a1a3ec54f492213606b
Tech_Mark <marksimiyu002@gmail.com>
2025-11-14 11:30:23 +0300

    Delete variable directory

--- variable/session.js
+++
@@ -1,9 +0,0 @@
-/**
- * ⚙️ Fill in your WhatsApp session ID here (from your session generator)
- * Example: "ABCD1234==:XYZ123"
- */
-module.exports = {
-  SESSION_ID: "PASTE_YOUR_SESSION_ID_HERE",
-  PREFIX: "!",
-  OWNER_NUMBER: "2547XXXXXXXX"
-};
