399472b1f4871ba04ba6d4f543fbb91a255471a0
Tech_Mark <marksimiyu002@gmail.com>
2025-11-14 11:30:43 +0300

    Delete anticall.js

--- anticall.js
+++
@@ -1,6 +0,0 @@
-module.exports.antiCall = async (bot, json) => {
-  try {
-    const from = json.from;
-    await bot.sendMessage(from, { text: "🚫 Sorry, calling the bot is not allowed!" });
-  } catch (err) { console.error(err); }
-};
